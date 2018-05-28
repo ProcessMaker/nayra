560ad19cb0b9b04fec0d9430d35229a7e277bdf6
David Callizaya <davidcallizaya@gmail.com>
2018-05-28 16:04:15 -0400

    Fix and update unit test for execution instance repository

--- tests/Feature/Engine/LoadExecutionInstancesTest.php
+++ tests/Feature/Engine/LoadExecutionInstancesTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Engine;
 use ProcessMaker\Nayra\Contracts\Bpmn\ActivityInterface;
 use ProcessMaker\Nayra\Contracts\Bpmn\EndEventInterface;
 use ProcessMaker\Nayra\Contracts\Bpmn\EventInterface;
+use ProcessMaker\Nayra\Contracts\Bpmn\GatewayInterface;
 use ProcessMaker\Nayra\Contracts\Bpmn\ProcessInterface;
 use ProcessMaker\Repositories\BpmnFileRepository;
 
@@ -21,7 +22,8 @@ class LoadExecutionInstancesTest extends EngineTestCase
      */
     private function prepareSequentialProcess()
     {
-        $this->executionInstanceRepository->setRawData([
+        $executionInstanceRepository = $this->engine->getRepositoryFactory()->getExecutionInstanceRepository();
+        $executionInstanceRepository->setRawData([
             'executionInstanceId' => [
                 'processId' => 'SequentialTask',
                 'data' => [],
@@ -41,7 +43,8 @@ class LoadExecutionInstancesTest extends EngineTestCase
      */
     private function prepareParallelProcess()
     {
-        $this->executionInstanceRepository->setRawData([
+        $executionInstanceRepository = $this->engine->getRepositoryFactory()->getExecutionInstanceRepository();
+        $executionInstanceRepository->setRawData([
             'otherExecutionInstanceId' => [
                 'processId' => 'ParallelProcess',
                 'data' => [],
@@ -49,30 +52,6 @@ class LoadExecutionInstancesTest extends EngineTestCase
                     [
                         'elementId' => 'task2',
                         'status' => ActivityInterface::TOKEN_STATE_ACTIVE,
-                    ],
-                    [
-                        'elementId' => 'task3',
-                        'status' => ActivityInterface::TOKEN_STATE_ACTIVE,
-                    ],
-                ],
-            ]
-        ]);
-    }
-
-    /**
-     * Set data for a parallel process with an activity in exception state
-     *
-     */
-    private function prepareParallelProcessWithException()
-    {
-        $this->executionInstanceRepository->setRawData([
-            'otherExecutionInstanceId' => [
-                'processId' => 'ParallelProcess',
-                'data' => [],
-                'tokens' => [
-                    [
-                        'elementId' => 'task2',
-                        'status' => ActivityInterface::TOKEN_STATE_FAILING,
                     ],
                     [
                         'elementId' => 'task3',
@@ -89,7 +68,8 @@ class LoadExecutionInstancesTest extends EngineTestCase
      */
     private function prepareParallelProcessWithActivityCompleted()
     {
-        $this->executionInstanceRepository->setRawData([
+        $executionInstanceRepository = $this->engine->getRepositoryFactory()->getExecutionInstanceRepository();
+        $executionInstanceRepository->setRawData([
             'otherExecutionInstanceId' => [
                 'processId' => 'ParallelProcess',
                 'data' => [],
@@ -101,6 +81,31 @@ class LoadExecutionInstancesTest extends EngineTestCase
                     [
                         'elementId' => 'task3',
                         'status' => ActivityInterface::TOKEN_STATE_ACTIVE,
+                    ],
+                ],
+            ]
+        ]);
+    }
+
+    /**
+     * Set data for a parallel process with an activity in exception state
+     *
+     */
+    private function prepareParallelProcessWithException()
+    {
+        $executionInstanceRepository = $this->engine->getRepositoryFactory()->getExecutionInstanceRepository();
+        $executionInstanceRepository->setRawData([
+            'otherExecutionInstanceId' => [
+                'processId' => 'ParallelProcess',
+                'data' => [],
+                'tokens' => [
+                    [
+                        'elementId' => 'task2',
+                        'status' => ActivityInterface::TOKEN_STATE_ACTIVE,
+                    ],
+                    [
+                        'elementId' => 'task3',
+                        'status' => ActivityInterface::TOKEN_STATE_FAILING,
                     ],
                 ],
             ]
@@ -178,6 +183,20 @@ class LoadExecutionInstancesTest extends EngineTestCase
 
         //Assertion: Second and third activity are completed and closed, the gateway is activiate, and the process ends.
         $this->assertEvents([
+            ActivityInterface::EVENT_ACTIVITY_COMPLETED,
+            ActivityInterface::EVENT_ACTIVITY_CLOSED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_ARRIVES,
+            ActivityInterface::EVENT_ACTIVITY_COMPLETED,
+            ActivityInterface::EVENT_ACTIVITY_CLOSED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_ARRIVES,
+            GatewayInterface::EVENT_GATEWAY_ACTIVATED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_CONSUMED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_CONSUMED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_PASSED,
+            EndEventInterface::EVENT_THROW_TOKEN_ARRIVES,
+            EndEventInterface::EVENT_THROW_TOKEN_CONSUMED,
+            EventInterface::EVENT_EVENT_TRIGGERED,
+            ProcessInterface::EVENT_PROCESS_COMPLETED,
         ]);
     }
 
@@ -204,18 +223,26 @@ class LoadExecutionInstancesTest extends EngineTestCase
         $secondActivity = $bpmnRepository->loadBpmElementById('task2');
         $thirdActivity = $bpmnRepository->loadBpmElementById('task3');
 
-        //Completes the second activity
-        $token = $secondActivity->getTokens($instance)->item(0);
-        $secondActivity->complete($token);
-        $this->engine->runToNextState();
-
         //Completes the third activity
         $token = $thirdActivity->getTokens($instance)->item(0);
         $thirdActivity->complete($token);
         $this->engine->runToNextState();
 
-        //Assertion: Second and third activity are completed and closed, the gateway is activiate, and the process ends.
+        //Assertion: Second activity is closed and the third activity are completed, then closed, the join gateway is activiate, and finally the process is completed.
         $this->assertEvents([
+            ActivityInterface::EVENT_ACTIVITY_CLOSED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_ARRIVES,
+            ActivityInterface::EVENT_ACTIVITY_COMPLETED,
+            ActivityInterface::EVENT_ACTIVITY_CLOSED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_ARRIVES,
+            GatewayInterface::EVENT_GATEWAY_ACTIVATED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_CONSUMED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_CONSUMED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_PASSED,
+            EndEventInterface::EVENT_THROW_TOKEN_ARRIVES,
+            EndEventInterface::EVENT_THROW_TOKEN_CONSUMED,
+            EventInterface::EVENT_EVENT_TRIGGERED,
+            ProcessInterface::EVENT_PROCESS_COMPLETED,
         ]);
     }
 
@@ -233,13 +260,12 @@ class LoadExecutionInstancesTest extends EngineTestCase
         $this->engine->setRepositoryFactory($bpmnRepository);
 
         //Set test data to load the sequential process
-        $this->prepareParallelProcessWithActivityCompleted();
+        $this->prepareParallelProcessWithException();
 
         //Load the execution instance
         $instance = $this->engine->loadExecutionInstance('otherExecutionInstanceId');
 
         //Get References by id
-        $secondActivity = $bpmnRepository->loadBpmElementById('task2');
         $thirdActivity = $bpmnRepository->loadBpmElementById('task3');
 
         //Assertion: The third activity is in falling state
