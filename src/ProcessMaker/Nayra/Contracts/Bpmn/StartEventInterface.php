ee11219844ca381b476d5891d851ab5312fc0f69
David Callizaya <davidcallizaya@gmail.com>
2020-04-28 13:13:47 -0400

    Declare excecute in start event interface

--- src/ProcessMaker/Nayra/Contracts/Bpmn/StartEventInterface.php
+++ src/ProcessMaker/Nayra/Contracts/Bpmn/StartEventInterface.php
@@ -19,4 +19,15 @@ interface StartEventInterface extends CatchEventInterface
      * @return $this;
      */
     public function start(ExecutionInstanceInterface $instance);
+
+    /**
+     * Method to be called when a message event arrives
+     *
+     * @param EventDefinitionInterface $event
+     * @param ExecutionInstanceInterface|null $instance
+     * @param TokenInterface|null $token
+     *
+     * @return $this
+     */
+    public function execute(EventDefinitionInterface $event, ExecutionInstanceInterface $instance = null, TokenInterface $token = null);
 }
