3b9f0e95b3f05da3e48696bbe21bba3c3b638c19
David Callizaya <davidcallizaya@gmail.com>
2020-10-21 17:54:11 -0400

    Add data from message

--- src/ProcessMaker/Nayra/Bpmn/StartEventTrait.php
+++ src/ProcessMaker/Nayra/Bpmn/StartEventTrait.php
@@ -114,8 +114,10 @@ trait StartEventTrait
         foreach ($this->getEventDefinitions() as $index => $eventDefinition) {
             if ($eventDefinition->assertsRule($event, $this, $instance, $token)) {
                 if ($instance === null) {
+                    $data = $token ? $token->getInstance()->getDataStore()->getData() : [];
                     $process = $this->getOwnerProcess();
                     $dataStorage = $process->getRepository()->createDataStore();
+                    $dataStorage->setData($data);
                     $instance = $process->getEngine()->createExecutionInstance($process, $dataStorage);
                 }
                 $this->triggerPlace[$index]->addNewToken($instance);
