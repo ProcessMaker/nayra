f2fc90256ed9d678ba3064e3607e2474ac5c0803
David Callizaya <davidcallizaya@gmail.com>
2021-02-11 17:45:30 -0400

    InputOutputSpecification Unit test

---
+++ tests/unit/ProcessMaker/Nayra/Bpmn/InputOutputSpecificationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ProcessMaker\Nayra\Bpmn;
+
+use PHPUnit\Framework\TestCase;
+use ProcessMaker\Nayra\Bpmn\Models\InputOutputSpecification;
+use ProcessMaker\Nayra\Bpmn\Models\InputSet;
+use ProcessMaker\Nayra\Bpmn\Models\OutputSet;
+
+/**
+ * Tests InputOutputSpecification class implementation
+ *
+ * @package ProcessMaker\Nayra\Bpmn
+ */
+class InputOutputSpecificationTest extends TestCase
+{
+    /**
+     * Tests that setters and getters are working properly
+     */
+    public function testSettersAndGetters()
+    {
+        // Create the objects that will be set in the data store
+        $ioSpecification = new InputOutputSpecification();
+
+        $dataOutput = new Collection();
+        $ioSpecification->setDataOutput($dataOutput);
+        $this->assertEquals($dataOutput, $ioSpecification->getDataOutput());
+
+        $dataInput = new Collection();
+        $ioSpecification->setDataInput($dataInput);
+        $this->assertEquals($dataInput, $ioSpecification->getDataInput());
+
+        $outputSet = new OutputSet();
+        $ioSpecification->setOutputSet($outputSet);
+        $this->assertEquals($outputSet, $ioSpecification->getOutputSet());
+
+        $inputSet = new InputSet();
+        $ioSpecification->setInputSet($inputSet);
+        $this->assertEquals($inputSet, $ioSpecification->getInputSet());
+    }
+}
