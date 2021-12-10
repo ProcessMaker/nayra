c8beea4ae7db7f44966786e0db4ccac5f8646828
David Callizaya <davidcallizaya@gmail.com>
2021-12-10 15:57:39 -0400

    Test Boundary with multiinstance CallActivity

--- tests/Feature/Engine/BoundaryEventTest.php
+++ tests/Feature/Engine/BoundaryEventTest.php
@@ -92,6 +92,78 @@ class BoundaryEventTest extends EngineTestCase
     }
 
     /**
+     * Tests a process with a signal boundary event attached to a task
+     */
+    public function testSignalBoundaryEventInMultiInstance()
+    {
+        // Load the process from a BPMN file
+        $bpmnRepository = new BpmnDocument();
+        $bpmnRepository->setEngine($this->engine);
+        $bpmnRepository->setFactory($this->repository);
+        $bpmnRepository->load(__DIR__ . '/files/Signal_BoundaryEvent_MultiInstance.bpmn');
+
+        // Get the process by Id
+        $process = $bpmnRepository->getProcess('ProcessId');
+        $dataStore = $this->repository->createDataStore();
+        $dataStore->setData([
+            'array' => [1,2],
+        ]);
+
+        // Create an instance of the process
+        $instance = $this->engine->createExecutionInstance($process, $dataStore);
+
+        // Get References
+        $start = $process->getEvents()->item(0);
+        $task1 = $bpmnRepository->getActivity('node_2');
+        $task2 = $bpmnRepository->getActivity('node_3');
+        $task3 = $bpmnRepository->getActivity('node_13');
+
+        // Start a process instance
+        $start->start($instance);
+        $this->engine->runToNextState();
+
+        // Assertion: The process is started and two tasks were activated
+        $this->assertEvents([
+            ProcessInterface::EVENT_PROCESS_INSTANCE_CREATED,
+            EventInterface::EVENT_EVENT_TRIGGERED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_ARRIVES,
+            GatewayInterface::EVENT_GATEWAY_ACTIVATED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_CONSUMED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_PASSED,
+            GatewayInterface::EVENT_GATEWAY_TOKEN_PASSED,
+            // 2 MultiInstance (task1) and 1 Trigger Task (task2)
+            ActivityInterface::EVENT_ACTIVITY_ACTIVATED,
+            ActivityInterface::EVENT_ACTIVITY_ACTIVATED,
+            ActivityInterface::EVENT_ACTIVITY_ACTIVATED,
+        ]);
+
+        // Complete second task
+        $task2->complete($task2->getTokens($instance)->item(0));
+        $this->engine->runToNextState();
+
+        // Assertion: Task 2 is completed, an end event Signal is thrown, then caught by the boundary event
+        $this->assertEvents([
+            ActivityInterface::EVENT_ACTIVITY_COMPLETED,
+            ActivityInterface::EVENT_ACTIVITY_CLOSED,
+            EndEventInterface::EVENT_THROW_TOKEN_ARRIVES,
+            SignalEventDefinitionInterface::EVENT_THROW_EVENT_DEFINITION,
+            BoundaryEventInterface::EVENT_BOUNDARY_EVENT_CATCH,
+            BoundaryEventInterface::EVENT_BOUNDARY_EVENT_CONSUMED,
+            EndEventInterface::EVENT_THROW_TOKEN_CONSUMED,
+            EndEventInterface::EVENT_EVENT_TRIGGERED,
+            ActivityInterface::EVENT_ACTIVITY_CANCELLED,
+            ActivityInterface::EVENT_ACTIVITY_ACTIVATED,
+
+        ]);
+
+        // Assertion: Task 1 does not have tokens
+        $this->assertEquals(0, $task1->getTokens($instance)->count());
+
+        // Assertion: Task 3 has one token
+        $this->assertEquals(1, $task3->getTokens($instance)->count());
+    }
+
+    /**
      * Tests a process with a cycle timer boundary event attached to a task
      */
     public function testCycleTimerBoundaryEvent()
